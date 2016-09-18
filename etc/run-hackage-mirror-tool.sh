271b54ee64bde09ff1e06bbad6afe0b8b9b9deb1
Michael Snoyman <michael@fpcomplete.com>
2016-09-18 13:37:29 +0000

    Include run-hackage-mirror-tool.sh

---
+++ etc/run-hackage-mirror-tool.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eux
+
+mkdir -p $HOME/workdir/logs
+cd $HOME/workdir/
+
+source /secret/config.sh
+
+export S3_ACCESS_KEY=$AWS_ACCESS_KEY_ID
+export S3_SECRET_KEY=$AWS_SECRET_ACCESS_KEY
+
+while true
+do
+    /usr/local/bin/hackage-mirror-tool +RTS -t -A2M -M256M -RTS \
+      --hackage-url      $HACKAGE_URL \
+      --hackage-pkg-url  $HACKAGE_URL/package/ \
+      --s3-base-url      https://s3.amazonaws.com \
+      --s3-bucket-id     $S3_BUCKET
+
+    sleep 5m
+done
